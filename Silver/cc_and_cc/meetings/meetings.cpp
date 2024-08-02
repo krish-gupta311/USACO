e4a9796514a6e455605559a4264e255ddcd31b8d
Krishang Gupta <64621081+krish-gupta311@users.noreply.github.com>
2024-08-02 19:55:09 +0000

    trying actual solution, with defines

--- Silver/cc_and_cc/meetings/meetings.cpp
+++ Silver/cc_and_cc/meetings/meetings.cpp
@@ -66,65 +66,71 @@
 using namespace std;
 
 typedef long long ll;
+typedef vector<int> vi; 
+typedef vector<pair<int, int>> vpi;
 
-struct Cow {
-    ll x;
-    int w, d;
-};
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define trav(a, x) for (auto& a: x)
+
+#define pb push_back
+#define rsz resize
+#define sz(x) int(x.size())
+#define all(x) begin(x), end(x)
+#define f first
+#define s second
 
 ifstream fin("meetings.in");
 ofstream fout("meetings.out");
 
-void readInput(int& N, int& L, vector<Cow>& cows) {
+void readInput(int& N, int& L, vi& x, vi& w, vi& d) {
     fin >> N >> L;
-    cows.resize(N);
-    for (int i = 0; i < N; i++) {
-        fin >> cows[i].x >> cows[i].w >> cows[i].d;
-    }
+    x.rsz(N);
+    w.rsz(N);
+    d.rsz(N);
+    F0R(i, N) fin >> x[i] >> w[i] >> d[i];
 }
 
-ll calculateTotalWeight(const vector<Cow>& cows) {
+ll calculateTotalWeight(const vi& w) {
     ll total_weight = 0;
-    for (const auto& cow : cows) {
-        total_weight += cow.w;
-    }
+    trav(weight, w) total_weight += weight;
     return total_weight;
 }
 
-ll determineT(int N, int L, const vector<Cow>& cows, ll total_weight) {
-    vector<pair<ll, int>> events;
-    for (const auto& cow : cows) {
-        if (cow.d == -1) {
-            events.emplace_back(cow.x, cow.w);
-        } else {
-            events.emplace_back(L - cow.x, cow.w);
-        }
+ll determineT(int N, int L, const vi& x, const vi& w, const vi& d, ll total_weight) {
+    vi lef, rig;
+    F0R(i, N) {
+        if (d[i] == -1) lef.pb(x[i]);
+        else rig.pb(x[i]);
     }
 
-    sort(events.begin(), events.end());
+    vpi events;
+    F0R(i, sz(lef)) events.pb({lef[i], w[i]});
+    F0R(i, sz(rig)) events.pb({L - rig[i], w[sz(lef) + i]});
 
+    sort(all(events));
     ll weight = total_weight;
-    for (const auto& event : events) {
-        weight -= 2 * event.second;
-        if (weight <= 0) {
-            return event.first;
-        }
+    trav(event, events) {
+        weight -= 2 * event.s;
+        if (weight <= 0) return event.f;
     }
     return 0;
 }
 
-int countMeetings(int N, ll T, const vector<Cow>& cows) {
+int countMeetings(int N, ll T, const vi& x, const vi& d) {
     queue<ll> right_moving_cows;
     int meetings = 0;
 
-    for (const auto& cow : cows) {
-        if (cow.d == 1) {
-            right_moving_cows.push(cow.x);
+    F0R(i, N) {
+        if (d[i] == 1) {
+            right_moving_cows.push(x[i]);
         } else {
-            while (!right_moving_cows.empty() && right_moving_cows.front() + 2 * T < cow.x) {
+            while (!right_moving_cows.empty() && right_moving_cows.front() + 2 * T < x[i]) {
                 right_moving_cows.pop();
             }
-            meetings += right_moving_cows.size();
+            meetings += sz(right_moving_cows);
         }
     }
     return meetings;
@@ -133,15 +139,23 @@ int countMeetings(int N, ll T, const vector<Cow>& cows) {
 int main() {
 
     int N, L;
-    vector<Cow> cows;
-    readInput(N, L, cows);
+    vi x, w, d;
+    readInput(N, L, x, w, d);
 
-    ll total_weight = calculateTotalWeight(cows);
-    ll T = determineT(N, L, cows, total_weight);
+    ll total_weight = calculateTotalWeight(w);
+    ll T = determineT(N, L, x, w, d, total_weight);
 
-    sort(cows.begin(), cows.end(), [](Cow a, Cow b) { return a.x < b.x; });
+    vpi cows(N);
+    F0R(i, N) cows[i] = {x[i], d[i]};
+    sort(all(cows), [](pair<int, int> a, pair<int, int> b) { return a.first < b.first; });
 
-    int meetings = countMeetings(N, T, cows);
+    vi x_sorted(N), d_sorted(N);
+    F0R(i, N) {
+        x_sorted[i] = cows[i].first;
+        d_sorted[i] = cows[i].second;
+    }
+
+    int meetings = countMeetings(N, T, x_sorted, d_sorted);
     fout << meetings << "\n";
 
     return 0;
