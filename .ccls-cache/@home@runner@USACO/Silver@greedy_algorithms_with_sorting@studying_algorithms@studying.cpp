264337bfe5089816e6aee664f3cce61be1dde0c9
Krishang Gupta <64621081+krish-gupta311@users.noreply.github.com>
2024-08-07 19:51:25 +0000

    trying solution code

--- .ccls-cache/@home@runner@USACO/Silver@greedy_algorithms_with_sorting@studying_algorithms@studying.cpp
+++ .ccls-cache/@home@runner@USACO/Silver@greedy_algorithms_with_sorting@studying_algorithms@studying.cpp
@@ -1,3 +1,31 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     int N, X;
+//     cin >> N >> X;
+
+//     vector<int> algorithms(N);
+//     for (int i = 0; i < N; i++) {
+//         cin >> algorithms[i];
+//     }
+//     sort(algorithms.begin(),algorithms.end());
+
+//     int time = 0;
+//     int ans = 0;
+//     while (ans < N) {
+//         time += algorithms[ans];
+//         if (time < X) {
+//             ans++;
+//         } else {
+//             break;
+//         }
+//     }
+
+//     cout << ans << endl;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
