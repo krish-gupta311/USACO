517d9da6e701e351b1ea91c05e113a46897a1b89
Krishang Gupta <64621081+krish-gupta311@users.noreply.github.com>
2024-08-14 18:03:52 +0000

    handling integer overflow

--- .ccls-cache/@home@runner@USACO/Silver@greedy_algorithms_with_sorting@rest_stops@reststops.cpp
+++ .ccls-cache/@home@runner@USACO/Silver@greedy_algorithms_with_sorting@rest_stops@reststops.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int,int> pii;
+typedef long long ll;
+typedef pair<ll,ll> pii;
 
 ifstream fin("reststops.in");
 ofstream fout("reststops.out");
 
 int main() {
 
-    int L,N,rf,rb;
+    ll L,N,rf,rb;
     fin >> L >> N >> rf >> rb;
 
     vector<pii> stops(N);
-    for (int i = 0; i < N; i++) {
+    for (ll i = 0; i < N; i++) {
         fin >> stops[i].first >> stops[i].second;
     }
 
     deque<pii> good_stops;
-    int max_tasty = 0;
-    for (int i = N - 1; i > -1; i--) {
+    ll max_tasty = 0;
+    for (ll i = N - 1; i > -1; i--) {
         if (stops[i].second > max_tasty) {
             good_stops.push_front(stops[i]);
             max_tasty = stops[i].second;
         }
     }
 
-    int last_stop = 0;
-    int ans = 0;
+    ll last_stop = 0;
+    ll ans = 0;
     for (const auto &stop : good_stops) {
         ans += stop.second*(stop.first - last_stop)*(rf-rb);
         last_stop = stop.first;
