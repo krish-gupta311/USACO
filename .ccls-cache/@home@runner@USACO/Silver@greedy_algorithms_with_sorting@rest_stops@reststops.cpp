f36ac1ed169b410ff21898ab74409e112eec442a
Krishang Gupta <64621081+krish-gupta311@users.noreply.github.com>
2024-08-14 17:57:09 +0000

    fixed logic after looking at solution

--- .ccls-cache/@home@runner@USACO/Silver@greedy_algorithms_with_sorting@rest_stops@reststops.cpp
+++ .ccls-cache/@home@runner@USACO/Silver@greedy_algorithms_with_sorting@rest_stops@reststops.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int,int> pii;
 
 ifstream fin("reststops.in");
 ofstream fout("reststops.out");
@@ -9,14 +10,23 @@ int main() {
     int L,N,rf,rb;
     fin >> L >> N >> rf >> rb;
 
-    vector<pair<int,int>> stops(N);
+    vector<pii> stops(N);
     for (int i = 0; i < N; i++) {
         fin >> stops[i].first >> stops[i].second;
     }
 
+    deque<pii> good_stops;
+    int max_tasty = 0;
+    for (int i = N - 1; i > -1; i--) {
+        if (stops[i].second > max_tasty) {
+            good_stops.push_front(stops[i]);
+            max_tasty = stops[i].second;
+        }
+    }
+
     int last_stop = 0;
     int ans = 0;
-    for (const auto &stop : stops) {
+    for (const auto &stop : good_stops) {
         ans += stop.second*(stop.first - last_stop)*(rf-rb);
         last_stop = stop.first;
     }
