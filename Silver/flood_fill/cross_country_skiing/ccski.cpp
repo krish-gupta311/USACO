7e4a9a8eececcf89b1c9713dd710ebb26eeaa2a9
Krishang Gupta <krishang.gupta.2009@gmail.com>
2025-05-17 10:17:45 -0400

    ccski final

--- Silver/flood_fill/cross_country_skiing/ccski.cpp
+++ Silver/flood_fill/cross_country_skiing/ccski.cpp
@@ -12,79 +12,91 @@ vector<vector<bool> > visited;
 
 void flood_fill(int x, int y, ll mid) {
 
-     if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size()) {
-         return;
-     }
-     if (visited[x][y]) {
-         return;
-     }
-     visited[x][y] = true;
-     if (abs(grid[x+1][y]-grid[x][y]) <= mid) flood_fill(x + 1, y, mid);
-     if (abs(grid[x-1][y]-grid[x][y]) <= mid) flood_fill(x - 1, y, mid);
-     if (abs(grid[x][y+1]-grid[x][y]) <= mid) flood_fill(x, y + 1, mid);
-     if (abs(grid[x][y-1]-grid[x][y]) <= mid) flood_fill(x, y - 1, mid);
+    if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size()) {
+        return;
+    }
+    if (visited[x][y]) {
+        return;
+    }
+    visited[x][y] = true;
 
- }
+    int dx[] = {-1, 1, 0, 0};
+    int dy[] = {0, 0, -1, 1};
 
- bool f(ll mid) {
+    for (int dir = 0; dir < 4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size()) {
+            if (!visited[nx][ny] && abs(grid[nx][ny] - grid[x][y]) <= mid) {
+                flood_fill(nx, ny, mid);
+            }
+        }
+    }
 
-     flood_fill(waypoints[0].first,waypoints[0].second,mid);
-     for (auto point : waypoints) {
-         if (!visited[point.first][point.second]) {
-             return false;
-         }
-     }
-     return true;
+}
 
- }
+bool f(ll mid) {
 
- ll first_true(ll lo, ll hi) {
+    for (int i = 0; i < visited.size(); ++i)
+        fill(visited[i].begin(), visited[i].end(), false);
 
-     lo--;
-     while (lo < hi) {
-         int mid = (lo + floor((hi-lo)/2));
-         if (f(mid)) {
-             hi = mid;
-         } else {
+    flood_fill(waypoints[0].first,waypoints[0].second,mid);
+    for (auto point : waypoints) {
+        if (!visited[point.first][point.second]) {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+ll first_true(ll lo, ll hi) {
+
+    lo--;
+    while (lo < hi) {
+        ll mid = lo + (hi - lo) / 2;
+        if (f(mid)) {
+            hi = mid;
+        } else {
              lo = mid + 1;
-         }
-         visited.resize(grid.size(),vector<bool>(grid[0].size(),false));
-     }
-     return lo;
+        }
+        visited.resize(grid.size(),vector<bool>(grid[0].size(),false));
+    }
+    return lo;
 
- }
+}
 
 
- int main() {
+int main() {
 
-     int N,M;
-     fin >> N >> M;
+    int N,M;
+    fin >> N >> M;
 
-     grid.resize(N,vector<ll>(M));
-     visited.resize(N,vector<bool>(M,false));
+    grid.resize(N,vector<ll>(M));
+    visited.resize(N,vector<bool>(M,false));
 
-     ll minA = __LONG_LONG_MAX__;
-     ll maxA = -__LONG_LONG_MAX__;
-     for (int i = 0; i < N; i++) {
-         for (int j = 0; j < M; j++) {
-             fin >> grid[i][j];
-             minA = min(minA,grid[i][j]);
-             maxA = max(maxA,grid[i][j]);
-         }
-     }
-     int maxD = maxA - minA;
+    ll minA = __LONG_LONG_MAX__;
+    ll maxA = -__LONG_LONG_MAX__;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            fin >> grid[i][j];
+            minA = min(minA,grid[i][j]);
+            maxA = max(maxA,grid[i][j]);
+        }
+    }
+    int maxD = maxA - minA;
 
-     for (int i = 0; i < N; i++) {
-         for (int j = 0; j < M; j++) {
-             int a;
-             fin >> a;
-             if (a == 1) waypoints.emplace_back(i,j);
-         }
-     }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            int a;
+            fin >> a;
+            if (a == 1) waypoints.emplace_back(i,j);
+        }
+    }
 
-     ll diff = first_true(0LL,maxD);
-     fout << diff << endl;
+    ll diff = first_true(0LL,maxD);
+    fout << diff << endl;
 
-     return 0;
+    return 0;
 
- }
+}
